f84dc09c1b31461a803662f9e2fa6880dd8e18fe
maxw7620 <maxw7620@ISS2TTPKH2.scdsb.pri>
2017-12-20 10:39:09 -0500

    did stuff

---
+++ src/assignment/LinkedPriorityQueue.java
@@ -0,0 +1,31 @@
+
+package assignment;
+
+public class LinkedPriorityQueue implements PriorityQueue{
+
+    @Override
+    public void enqueue(Object o, int priority) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object peekFront() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object dequeue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void enqueue(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
